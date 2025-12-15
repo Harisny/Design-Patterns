86b99730886f6f377da348f20ce7df24652f233c
Haris Nurryanto <harisnurryanto11625@gmail.com>
2025-12-15 21:08:25 +0700

    prototype beta

---
+++ prototype/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+    private String role;
+    private int age;
+
+    public User(String name, String role, int age) {
+        this.name = name;
+        this.role = role;
+        this.age = age;
+    }
+}
---
+++ prototype/UserProfile.java
@@ -0,0 +1,27 @@
+public class UserProfile implements Cloneable {
+    private String name;
+    private String role;
+    private Integer age;
+
+    public UserProfile(String name, String role, Integer age) {
+        this.name = name;
+        this.role = role;
+        this.age = age;
+    }
+
+    // Copy Construct
+    private UserProfile(UserProfile source) {
+        this.name = source.name;
+        this.role = source.role;
+        this.age = source.age;
+    }
+
+    public UserProfile clone() {
+        return new UserProfile(this);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + role + " - " + age;
+    }
+}
